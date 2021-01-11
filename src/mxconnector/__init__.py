9a7c1f969e8d35f1011802515c3d9ab4ba407440
mmiotto <mmiotto@br.ibm.com>
2021-01-11 14:49:26 -0300

    Code updates for pip install

---
+++ src/mxconnector/__init__.py
@@ -0,0 +1,16 @@
+# __all__ = ["MaximoConnector", "maxintclient", "OrderByClause", "OSResource", "schema", "selectclause", "service", "WhereClause"]
+# import mxconnector.MaximoConnector
+# import mxconnector.maxintclient
+# import mxconnector.OrderByClause
+# import mxconnector.OSResource
+# import mxconnector.schema
+# import mxconnector.selectclause
+# import mxconnector.service
+# import mxconnector.WhereClause
+from mxconnector.MaximoConnector import MaximoConnector
+from mxconnector.OrderByClause import OrderByClause
+from mxconnector.OSResource import OSResource
+from mxconnector.schema import Schema
+from mxconnector.selectclause import SelectClause
+from mxconnector.service import Service
+from mxconnector.WhereClause import WhereClause
